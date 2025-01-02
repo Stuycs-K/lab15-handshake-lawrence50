72f53f4610b82b69a352271c8ba770bbc121c098
lawrence <lshek50@stuy.edu>
2025-01-01 21:33:39 -0500

    server gets message from client and sends SYN_ACK, client receives and removes PP

--- pipe_networking.c
+++ pipe_networking.c
@@ -10,12 +10,11 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-  int from_client = 0;
   if (mkfifo(WKP, 0666) == -1) {
     perror("server create WKP fail");
     exit(1);
   }
-  from_client = open(WKP, O_RDONLY);
+  int from_client = open(WKP, O_RDONLY);
   if (from_client == -1) {
     perror("open WKP fail")
     exit(1);
@@ -35,9 +34,23 @@ int server_setup() {
   =========================*/
 int server_handshake(int *to_client) {
   int from_client = server_setup();
-  open(from_client, O_WRONLY);
+  // server gets message from client (PP name)
+  char buffer[HANDSHAKE_BUFFER_SIZE];
+  if (read(from_client, buffer, HANDSHAKE_BUFFER_SIZE) < 0) {
+    perror("server couldn't read PP name from client");
+    exit(1);
+  }
 
-  // send SYN ACK
+
+  // send SYN ACK to client
+  *to_client = open(buffer, O_WRONLY);
+  if (*to_client == -1) {
+    perror("server open PP failed");
+    exit(1);
+  }
+  int rand_int = rand(); 
+  sprintf(buffer, "%d", rand_int);
+  write(*to_client, buffer, strlen(buffer) + 1);
   return from_client;
 }
 
@@ -53,20 +66,37 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   // client creates private pipe with PID as name 
-  char PP_name[100];
+  char PP_name[HANDSHAKE_BUFFER_SIZE];
   sprintf(PP_name, "%d", getpid());
   if (mkfifo(PP_name, 0666) == -1) {
     perror("create PP fail");
     exit(1);
   }
-  // open WKP to write
-  int from_server = open(WKP, O_WRONLY);
-  if (from_server == -1) {
+  // open WKP (upstream) to write
+  to_server = open(WKP, O_WRONLY);
+  if (to_server == -1) {
     perror("client fail to open WKP");
     exit(1);
   }
-  // send PP name to server 
-  write(from_server, PP_name, strlen(PP_name) + 1);
+  // send PP name (SYN) to server 
+  write(to_server, PP_name, strlen(PP_name) + 1);
+
+  // client receives SYN ACK from server 
+  int from_server = open(PP_name, O_RDONLY);
+  if (from_server == -1) {
+    perror("client fail to open PP");
+    exit(1);
+  }
+  char buffer[HANDSHAKE_BUFFER_SIZE];
+  if (read(from_server, buffer, HANDSHAKE_BUFFER_SIZE) < 0) {
+    perror("client fail to read SYN ACK from server");
+    exit(1);
+  }
+  // remove PP
+  remove(PP_name);
+
+  // send final acknowledgement to server (same rand_int + 1)
+  
   return from_server;
 }
 
