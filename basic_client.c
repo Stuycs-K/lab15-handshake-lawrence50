2e0a6dd80123701210ac01f6aab5230b33dc5305
lawrence <lshek50@stuy.edu>
2025-01-07 01:09:20 -0500

    looping send messages

--- basic_client.c
+++ basic_client.c
@@ -6,24 +6,19 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
-  // write a byte to server
-  //char message = 'w';
-  //int bytesWritten = write(to_server, &message, sizeof(char));
-  //if (bytesWritten < 0) {
-  //  perror("client fail to write to server");
-  //  exit(1);
-  //}
-  //printf("client wrote %d bytes to server\n", bytesWritten);
-
-  // read a byte from server 
-  char buffer[BUFFER_SIZE];
-  int bytesRead = read(from_server, buffer, BUFFER_SIZE - 1);
-  if (bytesRead < 0) {
-    perror("client failed to read from server");
-    exit(1);
+  
+  while (1) {
+    char buffer[BUFFER_SIZE];
+    int bytesRead = read(from_server, buffer, BUFFER_SIZE - 1);
+    if (bytesRead < 0) {
+      perror("client failed to read from server");
+      exit(1);
+    }
+    printf("client received %s\n", buffer);
   }
-  printf("client read %d bytes from server\n", bytesRead);
+
 
   close(to_server);
   close(from_server);
 }
+
